6a8fe7f834b5d12afc4a48d37e7adf66a1628fce
sinyoung0403 <tlstjs753@gmail.com>
2025-04-07 21:32:27 +0900

    Feat: CommentLikes Entity, PostLikes Entity 추가 date: 2025/04/07

--- src/main/java/com/example/fakebookproject/entity/PostLikes.java
+++ src/main/java/com/example/fakebookproject/entity/PostLikes.java
@@ -1,4 +1,33 @@
 package com.example.fakebookproject.entity;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+
+@Getter
+@Entity
+@Table(name = "post_like")
+@NoArgsConstructor
 public class PostLikes {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @ManyToOne
+  @JoinColumn(name = "user_id")
+  private Users users;
+
+  @ManyToOne
+  @JoinColumn(name = "post_id")
+  private Posts posts;
+
+  public void setUsers(Users users) {
+    this.users = users;
+  }
+
+  public void setPosts(Posts posts) {
+    this.posts = posts;
+  }
 }
+
