5e55c96ab18ef4aef70f0a19ddf3af83c17898f7
sinyoung0403 <tlstjs753@gmail.com>
2025-04-07 15:46:11 +0900

    Chore: 초기 설정 date: 2025/04/07

---
+++ src/test/java/com/example/fakebookproject/FakebookProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fakebookproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FakebookProjectApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
