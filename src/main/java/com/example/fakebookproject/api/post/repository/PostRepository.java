a07a911936b5ce9d889359a279043495e14ed1ca
kibeo <kibeom0806@gmail.com>
2025-04-08 14:57:04 +0900

    feat: PostRepository -> JpaRepository date: 2025.04.08

--- src/main/java/com/example/fakebookproject/api/post/repository/PostRepository.java
+++ src/main/java/com/example/fakebookproject/api/post/repository/PostRepository.java
@@ -1,4 +1,7 @@
 package com.example.fakebookproject.api.post.repository;
 
-public class PostRepository {
+import com.example.fakebookproject.api.post.entity.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostRepository extends JpaRepository<Post, Long> {
 }
