6f3b2da1e8030d46140a54246494709d59269b94
kibeo <kibeom0806@gmail.com>
2025-04-08 15:11:09 +0900

    feat: PostRepository findByIdOrElseThrow date: 2025.04.08

--- src/main/java/com/example/fakebookproject/api/post/repository/PostRepository.java
+++ src/main/java/com/example/fakebookproject/api/post/repository/PostRepository.java
@@ -5,7 +5,6 @@ import com.example.fakebookproject.common.exception.CustomException;
 import com.example.fakebookproject.common.exception.ExceptionCode;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.NoSuchElementException;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
     default Post findByIdOrElseThrow(Long id) {
