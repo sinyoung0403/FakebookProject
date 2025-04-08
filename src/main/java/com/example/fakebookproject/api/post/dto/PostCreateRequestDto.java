213d29fc9a669f4d86f53ad3d98e074f0f5fa64e
kibeo <kibeom0806@gmail.com>
2025-04-08 15:38:40 +0900

    feat: createPost method date: 2025.04.08

--- src/main/java/com/example/fakebookproject/api/post/dto/PostCreateRequestDto.java
+++ src/main/java/com/example/fakebookproject/api/post/dto/PostCreateRequestDto.java
@@ -1,7 +1,6 @@
 package com.example.fakebookproject.api.post.dto;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
