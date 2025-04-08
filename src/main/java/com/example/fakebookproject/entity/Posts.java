15044a3a3eff8f69ceafbba0194bc29ac35bbed8
kibeo <kibeom0806@gmail.com>
2025-04-08 10:19:51 +0900

    feat: post entity date: 2025.04.08

--- src/main/java/com/example/fakebookproject/entity/Posts.java
+++ src/main/java/com/example/fakebookproject/entity/Posts.java
@@ -1,4 +1,45 @@
 package com.example.fakebookproject.entity;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Entity
+@Table(name="posts")
 public class Posts {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, columnDefinition = "longtext")
+    private String contents;
+
+    @Column
+    private String imageUrl;
+
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime modifiedAt;
+
+    @Column(nullable = false)
+    private Boolean isDeleted = Boolean.FALSE;
+
+    @Column(nullable = false)
+    private Long likesCount;
+
+    @ManyToOne
+    @JoinColumn(name="userId")
+    private Users users;
+
+
 }
