cd5eeec582aab7cb664272f334605006cff69256
exmrim <exmrim@naver.com>
2025-04-08 19:57:19 +0900

    Feat: Friend Repository 수정 date: 2025/04/08

--- src/main/java/com/example/fakebookproject/api/friend/repository/FriendRepository.java
+++ src/main/java/com/example/fakebookproject/api/friend/repository/FriendRepository.java
@@ -7,10 +7,17 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
+
 public interface FriendRepository extends JpaRepository<FriendStatus, Long> {
 
     Page<FriendStatus> findById(Long userId, Pageable pageable);
 
+    /**
+     * 내 친구 목록
+     * @param userId
+     * @return
+     */
     @Query("""
                     SELECT f, u FROM FriendStatus f
                     JOIN User u ON (
@@ -20,19 +27,32 @@ public interface FriendRepository extends JpaRepository<FriendStatus, Long> {
                     )
                     WHERE f.status = 1        
             """)
-    Page<Object[]> findAllByUserIdAndStatusAcceptedOrElseThrow(@Param("userId") Long userId, Pageable pageable);
+    List<Object[]> findAllByUserIdAndStatusAcceptedOrElseThrow(@Param("userId") Long userId);
 
+    /**
+     * 추천 친구 목록
+     * @param userId
+     * @param pageable
+     * @return
+     */
     @Query("""
                     SELECT f, u FROM FriendStatus f
                     JOIN User u ON (
                         (f.requestUser.id = :userId AND u.id = f.responseUser.id)
                         OR 
                         (f.responseUser.id = :userId AND u.id = f.requestUser.id)
-                    )
+                    ) 
+                    WHERE f.status = 0 AND u.cityName = :cityName
                         
             """)
     Page<Object[]> findRecommendationAllByUserIdOrElseThrow(@Param("userId") Long userId, Pageable pageable);
 
+    /**
+     * 요청 받은 친구 목록
+     * @param userId
+     * @param pageable
+     * @return
+     */
     @Query("""
                     SELECT f, u FROM FriendStatus f
                     JOIN User u ON (
