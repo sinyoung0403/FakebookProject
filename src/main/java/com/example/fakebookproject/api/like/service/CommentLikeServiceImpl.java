0bbe27dcb4f3cf236ec7e94efc114fb2fa2b378b
sinyoung0403 <tlstjs753@gmail.com>
2025-04-08 21:42:04 +0900

    Feat: Comment Like Service 구현 date: 2025/04/08 1. Create Service 구현 2. Read Service 구현 3. Delete Service 구현

--- src/main/java/com/example/fakebookproject/api/like/service/CommentLikeServiceImpl.java
+++ src/main/java/com/example/fakebookproject/api/like/service/CommentLikeServiceImpl.java
@@ -1,7 +1,9 @@
 package com.example.fakebookproject.api.like.service;
 
 import com.example.fakebookproject.api.comment.entity.Comment;
+import com.example.fakebookproject.api.comment.repository.CommentRepository;
 import com.example.fakebookproject.api.like.dto.FindAllCommentLikeResponseDto;
+import com.example.fakebookproject.api.like.entity.CommentLike;
 import com.example.fakebookproject.api.like.repository.CommentLikeRepository;
 import com.example.fakebookproject.api.post.entity.Post;
 import com.example.fakebookproject.api.post.repository.PostRepository;
@@ -10,6 +12,7 @@ import com.example.fakebookproject.api.user.entity.User;
 import com.example.fakebookproject.api.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -20,26 +23,44 @@ public class CommentLikeServiceImpl implements CommentLikeService {
     private final CommentLikeRepository commentLikeRepository;
     private final UserRepository userRepository;
     private final PostRepository postRepository;
+    private final CommentRepository commentRepository;
 
-
+    @Transactional
     @Override
-    public void createCommentLike(Long postId, Long commentId, LoginRequestDto loginRequestDto) {
-        // Data 검증
+    public void createCommentLike(Long postId, Long commentId, Long loginUserId) {
         // 1. 데이터 검증 및 조회
-        // Post 는 검증만 잘못된 URL 이라고만 알려주기.
-        User findUser = userRepository.findUserByEmailOrElseThrow(loginRequestDto.getEmail());
-//        Post findPost = postRepository.existsById(postId);
-//        Comment comment =
+        User findUser = userRepository.findUserByIdOrElseThrow(loginUserId);
+        postRepository.findPostByIdOrElseThrow(postId);
+        Comment findComment = commentRepository.findCommentByIdOrElseThrow(commentId);
 
+        // 2. Entity 변환
+        CommentLike commentLike = new CommentLike(findUser, findComment);
+
+        // 3. Comment 좋아요 추가
+        commentLikeRepository.save(commentLike);
+
+        // 4. Comment Table Like Count 추가
+        commentRepository.increaseLikeCount(commentId);
     }
 
     @Override
-    public List<FindAllCommentLikeResponseDto> findAllCommentLike(Long postId, Long commentId, LoginRequestDto loginRequestDto) {
-        return List.of();
+    public List<FindAllCommentLikeResponseDto> findAllCommentLike(Long postId, Long commentId, Long loginUserId) {
+        return commentLikeRepository.findAllByComment_Id(commentId)
+                .stream()
+                .map(FindAllCommentLikeResponseDto::from).toList();
     }
 
     @Override
-    public void deleteCommentLike(Long postId, Long commentId, LoginRequestDto loginRequestDto) {
+    public void deleteCommentLike(Long postId, Long commentId, Long loginUserId) {
+        // 1. 데이터 검증 및 조회
+        userRepository.validateNotExistenceByUserId(loginUserId);
+        postRepository.validateNotExistenceByPost_Id(postId);
+        commentRepository.validateNotExistenceByCommentId(commentId);
 
+        // 2. Entity 변환
+        CommentLike commentLike = commentLikeRepository.findByComment_IdAndUser_IdOrElseThrow(postId, loginUserId);
+
+        // 3. 삭제
+        commentLikeRepository.delete(commentLike);
     }
 }
