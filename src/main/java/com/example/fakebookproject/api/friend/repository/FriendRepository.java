7945df63c64d8502425a99aba2a71a2d17f0fa10
exmrim <exmrim@naver.com>
2025-04-08 14:45:51 +0900

    Feat: Friend 기능 추가 date: 2025/04/08

--- src/main/java/com/example/fakebookproject/api/friend/repository/FriendRepository.java
+++ src/main/java/com/example/fakebookproject/api/friend/repository/FriendRepository.java
@@ -1,4 +1,7 @@
 package com.example.fakebookproject.api.friend.repository;
 
-public interface FriendRepository {
+import com.example.fakebookproject.api.friend.entity.FriendStatus;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FriendRepository extends JpaRepository<FriendStatus, Long> {
 }
--- src/main/java/com/example/fakebookproject/api/friend/service/FriendService.java
+++ src/main/java/com/example/fakebookproject/api/friend/service/FriendService.java
@@ -1,4 +1,8 @@
 package com.example.fakebookproject.api.friend.service;
 
+import com.example.fakebookproject.api.friend.dto.FriendResponseDto;
+
 public interface FriendService {
+
+    FriendResponseDto requestFriend(Long requestUserId, Long responseUserId);
 }
