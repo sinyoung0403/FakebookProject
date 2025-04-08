9a35299186f01afd21097846c78c73d02ab778ca
sinyoung0403 <tlstjs753@gmail.com>
2025-04-08 21:43:16 +0900

    Style: 주석 변경 date: 2025/04/08

--- src/main/java/com/example/fakebookproject/api/like/dto/FindAllCommentLikeResponseDto.java
+++ src/main/java/com/example/fakebookproject/api/like/dto/FindAllCommentLikeResponseDto.java
@@ -1,5 +1,6 @@
 package com.example.fakebookproject.api.like.dto;
 
+import com.example.fakebookproject.api.like.entity.CommentLike;
 import com.example.fakebookproject.api.like.entity.PostLike;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -12,10 +13,11 @@ public class FindAllCommentLikeResponseDto {
 
     /**
      * Comment Like 를 ResponseDto 로 변환해주는 정적 메서드
-     * @param postLike
+     *
+     * @param commentLike
      * @return
      */
-    public static FindAllCommentLikeResponseDto from(PostLike postLike) {
-        return new FindAllCommentLikeResponseDto(postLike.getUser().getImageUrl(), postLike.getUser().getUserName());
+    public static FindAllCommentLikeResponseDto from(CommentLike commentLike) {
+        return new FindAllCommentLikeResponseDto(commentLike.getUser().getImageUrl(), commentLike.getUser().getUserName());
     }
 }
