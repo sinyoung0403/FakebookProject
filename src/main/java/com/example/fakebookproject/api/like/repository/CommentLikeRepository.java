3842474873271a07b7cdd527fe49f81c41648852
sinyoung0403 <tlstjs753@gmail.com>
2025-04-08 21:41:49 +0900

    Feat: Comment Like Repository 구현 date: 2025/04/08 1. findAllByComment_Id 2. findByComment_IdAndUser_Id

--- src/main/java/com/example/fakebookproject/api/like/repository/CommentLikeRepository.java
+++ src/main/java/com/example/fakebookproject/api/like/repository/CommentLikeRepository.java
@@ -1,7 +1,21 @@
 package com.example.fakebookproject.api.like.repository;
 
 import com.example.fakebookproject.api.like.entity.CommentLike;
+import com.example.fakebookproject.api.like.entity.PostLike;
+import com.example.fakebookproject.common.exception.CustomException;
+import com.example.fakebookproject.common.exception.ExceptionCode;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface CommentLikeRepository extends JpaRepository<CommentLike, Long> {
+
+    List<CommentLike> findAllByComment_Id(Long commentId);
+
+    Optional<CommentLike> findByComment_IdAndUser_Id(Long postId, Long userId);
+
+    default CommentLike findByComment_IdAndUser_IdOrElseThrow(Long postId, Long userId) {
+        return findByComment_IdAndUser_Id(postId, userId).orElseThrow(() -> new CustomException(ExceptionCode.NOT_FOUND_POST));
+    }
 }
