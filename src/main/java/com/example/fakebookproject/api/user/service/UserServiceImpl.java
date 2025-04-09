921d51930d8f4c17262053c7ee369a25a55527cc
thezz9 <thezz9@naver.com>
2025-04-09 19:19:40 +0900

    Style: 주석 추가 및 리턴 방식 변경 date: 2025/04/09 1. throws 주석에 에러코드 명시 2. 클라이언트에게 ResponseEntity 객체 생성없이 리턴하는 방식으로 변경

--- src/main/java/com/example/fakebookproject/api/user/service/UserServiceImpl.java
+++ src/main/java/com/example/fakebookproject/api/user/service/UserServiceImpl.java
@@ -22,7 +22,7 @@ public class UserServiceImpl implements UserService {
      *
      * @param dto 회원가입 요청 데이터 (이메일, 비밀번호, 이름 등)
      * @return 생성된 사용자 정보
-     * @throws CustomException 이미 존재하는 이메일인 경우 예외 발생
+     * @throws CustomException 이미 존재하는 이메일인 경우 예외 발생 (DUPLICATE_EMAIL)
      */
     @Override
     public UserResponseDto createUser(UserCreateRequestDto dto) {
@@ -46,7 +46,7 @@ public class UserServiceImpl implements UserService {
      *
      * @param userId 조회 대상 사용자 ID
      * @return 조회된 사용자 정보
-     * @throws CustomException 사용자 정보가 존재하지 않을 경우 예외 발생
+     * @throws CustomException 사용자 정보가 존재하지 않을 경우 예외 발생 (NOT_FOUND_USER)
      */
     @Override
     public UserResponseDto findUserById(Long userId) {
@@ -63,8 +63,8 @@ public class UserServiceImpl implements UserService {
      * @param loginUserId loginUser 세션에 저장된 로그인 사용자 ID
      * @param dto 비밀번호 요청 데이터 (사용자 본인 확인용)
      * @return 로그인한 사용자 정보
-     * @throws CustomException 사용자 정보가 존재하지 않을 경우 예외 발생
-     * @throws CustomException 비밀번호 불일치 시 예외 발생
+     * @throws CustomException 사용자 정보가 존재하지 않을 경우 예외 발생 (NOT_FOUND_USER)
+     * @throws CustomException 비밀번호 불일치 시 예외 발생 (INVALID_PASSWORD)
      */
     @Override
     public UserResponseDto findUserByLoginUserId(Long loginUserId, PasswordRequestDto dto) {
@@ -86,8 +86,8 @@ public class UserServiceImpl implements UserService {
      * @param loginUserId 세션에 저장된 로그인 사용자 ID
      * @param dto 사용자 수정 요청 데이터 (비밀번호 포함)
      * @return 수정된 사용자 정보
-     * @throws CustomException 사용자 정보가 존재하지 않을 경우 예외 발생
-     * @throws CustomException 비밀번호 불일치 시 예외 발생
+     * @throws CustomException 사용자 정보가 존재하지 않을 경우 예외 발생 (NOT_FOUND_USER)
+     * @throws CustomException 비밀번호 불일치 시 예외 발생 (INVALID_PASSWORD)
      */
     @Transactional
     @Override
@@ -116,6 +116,8 @@ public class UserServiceImpl implements UserService {
      *
      * @param loginUserId 세션에 저장된 로그인 사용자 ID
      * @param dto 비밀번호 요청 데이터 (사용자 본인 확인용)
+     * @throws CustomException 사용자 정보가 존재하지 않을 경우 예외 발생 (NOT_FOUND_USER)
+     * @throws CustomException 비밀번호 불일치 시 예외 발생 (INVALID_PASSWORD)
      */
     @Override
     public void deleteUser(Long loginUserId, PasswordRequestDto dto) {
@@ -136,7 +138,7 @@ public class UserServiceImpl implements UserService {
      *
      * @param dto 로그인 요청 정보 (이메일, 비밀번호)
      * @return 인증된 사용자 객체
-     * @throws CustomException 로그인 실패 시 예외 발생 (이메일 미존재 또는 비밀번호 불일치)
+     * @throws CustomException 이메일 미존재 또는 비밀번호 불일치 시 예외 발생 (LOGIN_FAILED)
      */
     @Override
     public User loginUser(LoginRequestDto dto) {
