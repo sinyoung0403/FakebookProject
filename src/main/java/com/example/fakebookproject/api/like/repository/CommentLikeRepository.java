0f811e13bbb3f20563fa21b7a0a08038a630187e
sinyoung0403 <tlstjs753@gmail.com>
2025-04-09 11:36:06 +0900

    Style: CommentLike Repository 주석 수정 date: 2025/04/09 1. default 함수 주석 수정

--- src/main/java/com/example/fakebookproject/api/like/repository/CommentLikeRepository.java
+++ src/main/java/com/example/fakebookproject/api/like/repository/CommentLikeRepository.java
@@ -15,18 +15,37 @@ public interface CommentLikeRepository extends JpaRepository<CommentLike, Long> 
 
     Optional<CommentLike> findByComment_IdAndUser_Id(Long postId, Long userId);
 
+    /**
+     * 주어진 commentId와 userId에 해당하는 엔티티를 조회하고, 존재하지 않으면 예외를 발생시킵니다.
+     *
+     * @param postId
+     * @param userId
+     * @return commentId와 userId 가 일치하는 CommentLike, 존재하지 않으면 예외
+     */
     default CommentLike findByComment_IdAndUser_IdOrElseThrow(Long postId, Long userId) {
         return findByComment_IdAndUser_Id(postId, userId).orElseThrow(() -> new CustomException(ExceptionCode.NOT_FOUND_POST));
     }
 
     boolean existsByUser_Id(Long userId);
 
+    /**
+     * userId 가 존재하는지 확인하기 위한 메서드
+     * Comment Like Repository 에 주어진 userId를 가진 엔티티가 존재하지 않으면 예외 발생
+     *
+     * @param userId
+     */
     default void validateExistenceByUserId(Long userId) {
         if (!existsByUser_Id(userId)) {
             throw new CustomException(ExceptionCode.NOT_FOUND_USER);
         }
     }
 
+    /**
+     * userId 가 존재하지 않는 걸 확인하기 위한 메서드
+     * Comment Like Repository 에 주어진 userId를 가진 엔티티가 존재하면 예외 발생
+     *
+     * @param userId
+     */
     default void validateNotExistenceByUserId(Long userId) {
         if (existsByUser_Id(userId)) {
             throw new CustomException(ExceptionCode.USER_ALREADY_EXISTS);
