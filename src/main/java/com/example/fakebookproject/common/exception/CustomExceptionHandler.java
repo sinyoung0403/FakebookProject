ae626fe4386f940c6694c7b6f295dfb7151dbcc8
sinyoung0403 <tlstjs753@gmail.com>
2025-04-10 13:06:17 +0900

    Style: 필요없는 부분 삭제 date: 2025/04/10

--- src/main/java/com/example/fakebookproject/common/exception/CustomExceptionHandler.java
+++ src/main/java/com/example/fakebookproject/common/exception/CustomExceptionHandler.java
@@ -1,10 +1,13 @@
 package com.example.fakebookproject.common.exception;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
 
 @RestControllerAdvice
 public class CustomExceptionHandler {
