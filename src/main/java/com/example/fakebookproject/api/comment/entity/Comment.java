c3b787feb386a9aee730df47ba07b8a631a92b7e
rang <withghd@gmail.com>
2025-04-10 19:02:57 +0900

    refactor: CustomBaseTimeEntity date: 2025/04/10 1. content 변경 시에만 updatedAt 변경되도록 수정

--- src/main/java/com/example/fakebookproject/api/comment/entity/Comment.java
+++ src/main/java/com/example/fakebookproject/api/comment/entity/Comment.java
@@ -2,20 +2,22 @@ package com.example.fakebookproject.api.comment.entity;
 
 import com.example.fakebookproject.api.post.entity.Post;
 import com.example.fakebookproject.api.user.entity.User;
-import com.example.fakebookproject.common.config.BaseTimeEntity;
+import com.example.fakebookproject.common.config.CustomBaseTimeEntity;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.SQLRestriction;
 
+import java.time.LocalDateTime;
+
 @Getter
 @Entity
 @NoArgsConstructor
+@Table(name = "comments")
 @SQLDelete(sql = "UPDATE comments SET is_deleted = true WHERE id = ?")
 @SQLRestriction("is_deleted = false")
-@Table(name = "comments")
-public class Comment extends BaseTimeEntity {
+public class Comment extends CustomBaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -38,6 +40,9 @@ public class Comment extends BaseTimeEntity {
     @JoinColumn(name = "post_id", nullable = false)
     private Post post;
 
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt = LocalDateTime.now();
+
     public Comment(User user, Post post, String content) {
         this.user = user;
         this.post = post;
@@ -47,5 +52,6 @@ public class Comment extends BaseTimeEntity {
 
     public void updateContent(String content) {
         this.content = content;
+        updatedAt = LocalDateTime.now();
     }
 }
